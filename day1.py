2993735c288734d55d9d0fda42b37eb84757b6fa
gemvcnte <111511069+gemvcnte@users.noreply.github.com>
2023-06-30 00:26:07 +0800

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,7 @@
+num1 = 5
+num2 = 3
+
+print('Sum is ' + format(num1 + num2))
+print('Subtract is ' + format(num1 - num2))
+print('Difference is ' + format(num1 * num2))
+print('Quotient is ' + format(num1 / num2))
