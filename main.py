f438fd92817d897bf9c0734d600411e4fa07b5de
gemvcnte <111511069+gemvcnte@users.noreply.github.com>
2023-07-06 13:35:18 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Programmer: Joshua Gem Vicente\nDate Created: 7/6/2023\nDescription of the Program: About\nFirst Name: Joshua Gem\nMiddle Name: Nugoy\nLast Name: Vicente\nCourse: BSIT\nYear: 3rd\nAge: 20\nGender: M\nCivil Status: Single\nNationality: Filipino\nMotto: abububwebwe")
+
+num1 = 5
+num2 = 8
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+print("Sum = {0}".format(sum))
+print("Difference = {0}".format(difference))
+print("Product = {0}".format(product))
+print("Quotient = {0}".format(quotient))
